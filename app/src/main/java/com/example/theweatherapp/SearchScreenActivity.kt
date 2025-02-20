f8e6226be0eabfaa105a77e3bcdd3fb66d2e6364
ebruaeo <ebruaeo@hotmail.com>
2025-02-20 18:00:34 +0300

    search button setOnClickListener and openKeyboard function added

--- app/src/main/java/com/example/theweatherapp/SearchScreenActivity.kt
+++ app/src/main/java/com/example/theweatherapp/SearchScreenActivity.kt
@@ -1,20 +1,42 @@
 package com.example.theweatherapp
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.theweatherapp.databinding.ActivitySearchScreenBinding
 
 class SearchScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_search_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivitySearchScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.searchButton.setOnClickListener {
+            binding.searchText.requestFocus()
+            openKeyboard()
+        }
+
+
+
+    }
+
+    fun openKeyboard() {
+        val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.searchText, InputMethodManager.SHOW_IMPLICIT)
     }
 }
