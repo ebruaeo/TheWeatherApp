b7fa13ecd955026600188d8a166c14bbbf7e0e9a
ebruaeo <ebruaeo@hotmail.com>
2025-03-03 03:26:46 +0300

    initializing adapter and observing weekly weather

--- app/src/main/java/com/example/theweatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/theweatherapp/presentation/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.example.theweatherapp.presentation
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.Manifest
-import android.annotation.SuppressLint
-import android.location.Location
-import android.util.Log
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import com.example.theweatherapp.R
 import com.example.theweatherapp.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -27,8 +27,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val LOCATION_PERMISSION_REQUEST_CODE = 1001
-
     private val viewModel by viewModels<WeatherViewModel>()
+    private val adapter = Adapter()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +44,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SearchScreenActivity::class.java)
             startActivity(intent)
         }
+
+        binding.recyclerView.adapter = adapter
     }
 
     private fun handleLocationPermission() {
@@ -74,17 +77,28 @@ class MainActivity : AppCompatActivity() {
     private fun setWindow() {
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
             insets
         }
         window.navigationBarColor = getResources().getColor(R.color.black)
     }
 
     private fun observeViewModel() {
+        observeCurrentWeather()
+        observeWeeklyWeather()
+    }
+
+    private fun observeCurrentWeather() {
         viewModel.weather.observe(this) {
             binding.run {
                 cityTextView.text = it.cityName
-                degreeTextView.text = it.currentWeatherDetails?.temp?.roundToInt().toString() + "°"
+                degreeTextView.text =
+                    it.currentWeatherDetails?.temp?.roundToInt().toString() + "°"
                 explanationTextView.text =
                     capitalizeEachWord(it.currentWeatherList?.first()?.description)
                 highAndLowTextView.text =
@@ -93,6 +107,15 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun observeWeeklyWeather() {
+        viewModel._weeklyWeather.observe(this) { response ->
+            if (response.list != null) {
+                adapter?.weeklyWeatherList = response.list
+            }
+        }
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -123,10 +146,20 @@ class MainActivity : AppCompatActivity() {
                 Log.d("Location", "Lat: $lat, Lon: $lon")
 
                 viewModel.fetchWeather(lat, lon)
-
+                viewModel.fetchWeeklyWeather(lat, lon)
+            } else {
+                Toast.makeText(
+                    this,
+                    "Unknown location",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }.addOnFailureListener {
-            Toast.makeText(this, "Failed to get location: ${it.message}", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Failed to get location: ${it.message}",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
