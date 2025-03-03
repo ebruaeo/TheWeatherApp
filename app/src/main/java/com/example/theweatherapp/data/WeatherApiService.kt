80cade34574c29e9c4478f439b421a47185ac99a
ebruaeo <ebruaeo@hotmail.com>
2025-03-03 03:28:23 +0300

    suspend fun for fetching weekly weather

--- app/src/main/java/com/example/theweatherapp/data/WeatherApiService.kt
+++ app/src/main/java/com/example/theweatherapp/data/WeatherApiService.kt
@@ -1,6 +1,7 @@
 package com.example.theweatherapp.data
 
 import com.example.theweatherapp.data.models.CurrentWeatherResponse
+import com.example.theweatherapp.data.models.WeeklyWeatherResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -14,6 +15,21 @@ interface WeatherApiService {
         @Query("appid") apiKey: String,
         @Query("units") units: String = "metric"
     ): Response<CurrentWeatherResponse>
+
+
+    @GET("forecast")
+    suspend fun getWeeklyWeather(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("appid") apiKey: String,
+        @Query("cnt") numberOfDays: Int = 7,
+        @Query("units") units: String = "metric"
+    ): Response<WeeklyWeatherResponse>
+
+
+
+
+
 
 
 
